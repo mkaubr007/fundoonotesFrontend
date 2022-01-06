74c77954c593524da0f1bcc05dbbec1359871332
mkaubr007 <mkaubr007@gmail.com>
2022-01-06 21:50:04 +0530

    feat:Added signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SignupComponent } from './components/signup/signup.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
